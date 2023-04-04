349d6ef27988245d0dc88ed925363bfe9e906ff7
Lee Suyoung <78334910+twotwobread@users.noreply.github.com>
2023-04-04 14:38:42 +0900

    [level 1] Title: 로또의 최고 순위와 최저 순위, Time: 0.03 ms, Memory: 78.3 MB -BaekjoonHub

--- 프로그래머스/lv1/77484. 로또의 최고 순위와 최저 순위/로또의 최고 순위와 최저 순위.java
+++ 프로그래머스/lv1/77484. 로또의 최고 순위와 최저 순위/로또의 최고 순위와 최저 순위.java
@@ -1,25 +1,22 @@
 class Solution {
+
+    private int[] ranking = {6, 6, 5, 4, 3, 2, 1, 1, 1, 1};
+
     public int[] solution(int[] lottos, int[] win_nums) {
-        int result = 0;
+        int nowCnt = 0;
         int zeroCnt = 0;
         for (int i = 0; i < lottos.length; i++) {
             if (lottos[i] == 0) zeroCnt += 1;
             for (int j = 0; j < win_nums.length; j++) {
-                if (lottos[i] == win_nums[j]) {
-                    result += 1;
+                if (win_nums[i] == lottos[j]) {
+                    nowCnt += 1;
                     break;
                 }
             }
         }
-        int maxValue = Math.max(0, zeroCnt);
-        int minValue = Math.min(0, zeroCnt);
-        int maxRank = 7 - ((result + maxValue) > 6 ? 6 : (result + maxValue)) ;
-        int minRank = 7 - ((result + minValue) > 6 ? 6 : (result + minValue));
-        
-        if (maxRank > 6) maxRank = 6;
-        if (minRank <= 0) minRank = 1;
-        System.out.println(String.format("최소 최대 : %s %s", minValue, maxValue));
-        System.out.println(String.format("제로 결과 : %s %s", zeroCnt, result));
-        return new int[] {maxRank <= 0 ? 1 : maxRank, minRank > 6 ? 6 : minRank};
+
+        int maxResult = Math.max(0, zeroCnt) + nowCnt;
+        int minResult = Math.min(0, zeroCnt) + nowCnt;
+        return new int[] {ranking[maxResult], ranking[minResult]};
     }
 }
