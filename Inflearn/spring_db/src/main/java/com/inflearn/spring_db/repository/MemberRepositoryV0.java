d366429bf695ce8e63134624a3192cc761efb0d6
Lee Suyoung <78334910+twotwobread@users.noreply.github.com>
2023-04-19 17:41:59 +0900

    [Chapter 1 JDBC 이해 끝] crud를 DriverManager를 사용해서 구현하고 이에 맞는 test 구현 -> 이를 통해서 JDBC를 이용하여 직접 DB와의 통신 과정을 구현해봤음.

--- Inflearn/spring_db/src/main/java/com/inflearn/spring_db/repository/MemberRepositoryV0.java
+++ Inflearn/spring_db/src/main/java/com/inflearn/spring_db/repository/MemberRepositoryV0.java
@@ -2,7 +2,9 @@ package com.inflearn.spring_db.repository;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.NoSuchElementException;
 
 import com.inflearn.spring_db.connection.DBConnectionUtil;
 import com.inflearn.spring_db.domain.Member;
@@ -32,10 +34,71 @@ public class MemberRepositoryV0 {
 
 	}
 
-	private static Connection getConnection() {
+	public Member findById(String memberId) {
+		String sql = "select * from member where member_id = ?";
+
+		try (
+			Connection conn = getConnection();
+			PreparedStatement preparedStatement = conn.prepareStatement(sql);
+			) {
+			preparedStatement.setString(1, memberId);
+			return convertMember(preparedStatement.executeQuery());
+		} catch (SQLException e) {
+			throw new RuntimeException(e);
+		}
+	}
+
+	public void update(String memberId, int money) throws SQLException{
+		String sql = "update member set money=? where member_id=?";
+
+		try(
+			Connection conn = getConnection();
+			PreparedStatement preparedStatement = conn.prepareStatement(sql);
+			) {
+			preparedStatement.setInt(1, money);
+			preparedStatement.setString(2, memberId);
+			int resultSize = preparedStatement.executeUpdate();
+			log.info("resultSize={}", resultSize);
+		} catch (SQLException e) {
+			log.error("db error", e);
+			throw e;
+		}
+	}
+
+	public void delete(String memberId) throws SQLException {
+		String sql = "delete from member where member_id=?";
+
+		try(
+			Connection conn = getConnection();
+			PreparedStatement preparedStatement = conn.prepareStatement(sql);
+		) {
+			preparedStatement.setString(1, memberId);
+			int resultSize = preparedStatement.executeUpdate();
+			log.info("resultSize={}", resultSize);
+		} catch (SQLException e) {
+			log.error("db error", e);
+			throw e;
+		}
+	}
+
+	private Member convertMember(ResultSet rs) throws SQLException {
+		if (rs.next()) {
+			Member member = new Member();
+			member.setMemberId(rs.getString("member_id"));
+			member.setMoney(rs.getInt("money"));
+			return member;
+		}
+		throw new NoSuchElementException("member not found member");
+	}
+
+	private Connection getConnection() {
 		return DBConnectionUtil.getConnection();
 	}
 }
 
 // SQL injection 공격을 예방하기 위해선 PreparedStatement를 통한 파라미터 바인딩 방식 이용 -> 기존의 방식으로 직접 값을 넣는다면 값 대신 다른 SQL을 삽압하여 데이터를 털 수 있음.
 // 이걸 막기 위해서 PreparedStatement에서는 들어오는 값을 SQL로 취급하지 않게 그냥 문자열 값으로 인식하게 구현하여 이를 해결했음.
+
+// ResultSet은 키-값 형태로 생긴 테이블형 자료구조임.
+// 최초의 커서는 데이터를 가르키지 않고 있기에 next()를 이용해서 다음 데이터로 커서를 옮겨서 값이 존재하는지를 확인하고 있으면 column명을 이용해서 값을 찾아옴.
+// iterator처럼 쓰면됨. -> 데이터가 없으면 false 반환함. -> while 형시;ㄱ으로 구현해도 됨. -> findById는 무조건 1개만 반환하니까 if로 했음.
--- Inflearn/spring_db/src/test/java/com/inflearn/spring_db/repository/MemberRepositoryV0Test.java
+++ Inflearn/spring_db/src/test/java/com/inflearn/spring_db/repository/MemberRepositoryV0Test.java
@@ -1,21 +1,62 @@
 package com.inflearn.spring_db.repository;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.assertj.core.api.Assertions.*;
 
 import java.sql.SQLException;
+import java.util.NoSuchElementException;
 
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
 
 import com.inflearn.spring_db.domain.Member;
 
-class MemberRepositoryV0Test {
 
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+class MemberRepositoryV0Test {
+	private static final String MEMBER_ID = "memberTest";
+	private static final Member MEMBER = new Member(MEMBER_ID, 10000);
 	MemberRepositoryV0 repo = new MemberRepositoryV0();
 
 	@Test
-	void crud() throws SQLException {
-		Member member = new Member("memberV1", 10000);
-		repo.save(member);
+	@Order(1)
+	void create() throws SQLException {
+		repo.save(MEMBER);
 	}
 
+	@Test
+	@Order(2)
+	void read() {
+		Member findMember = repo.findById(MEMBER_ID);
+		assertThat(findMember).isNotNull();
+		assertThat(findMember).isEqualTo(MEMBER);
+	}
+
+	@Test
+	@Order(3)
+	void update() throws SQLException {
+		// given
+		int updateMoney = 20000;
+
+		// when
+		repo.update(MEMBER_ID, updateMoney);
+		Member findMember = repo.findById(MEMBER_ID);
+
+		// then
+		assertThat(findMember).isNotNull();
+		assertThat(findMember.getMoney()).isEqualTo(updateMoney);
+	}
+
+	@Test
+	@Order(4)
+	void delete() throws SQLException {
+		repo.delete(MEMBER_ID);
+		assertThatThrownBy(() -> repo.findById(MEMBER_ID))
+			.isInstanceOf(NoSuchElementException.class);
+	}
 }
+
+// 테스트에서 반복해서 테스트할 수 있는 환경을 만드는 것이 중요함.
+// 이 테스트는 좋지 않은 테스트임. -> 이 테스트는 Order에 해당하는 모든 테스트가 돌아가야함. -> 예외가 터져서 delete가 동작하지 않는다면??
+// 자동으로 동작되는 테스트가 되지 않음. -> 저장은 했는데 삭제가 안되기 때문. -> 트랜잭션을 활용해서 이 문제를 깔끔하게 해결가능 -> 뒤에서 알아보자.
