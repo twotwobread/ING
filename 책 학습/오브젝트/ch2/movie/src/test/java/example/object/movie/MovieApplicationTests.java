c8206a0ab536a607a13520e55ac4608768e6713e
Lee Suyoung <78334910+twotwobread@users.noreply.github.com>
2023-04-28 01:05:15 +0900

    [오브젝트 ch2 프로젝트 생성] 프로젝트 생성

---
+++ 책 학습/오브젝트/ch2/movie/src/test/java/example/object/movie/MovieApplicationTests.java
@@ -0,0 +1,13 @@
+package example.object.movie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
