878cd39149af3fe958198d7273627e39d0c18fba
Lee Suyoung <78334910+twotwobread@users.noreply.github.com>
2023-05-03 21:52:20 +0900

    [오브젝트 ch5 책임 할당하기] 프로젝트 초기설정하기

---
+++ 책 학습/오브젝트/ch5/ch5/src/test/java/example/object/ch5/Ch5ApplicationTests.java
@@ -0,0 +1,13 @@
+package example.object.ch5;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ch5ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
