f8f004eed4edf6c35e60958c188fe288da4c0bfc
Lee Suyoung <78334910+twotwobread@users.noreply.github.com>
2023-06-05 16:10:17 +0900

    [Gold V] Title: Moo 게임, Time: 124 ms, Memory: 14172 KB -BaekjoonHub

---
+++ 백준/Gold/5904. Moo 게임/Moo 게임.java
@@ -0,0 +1,40 @@
+// S(k) = S(k - 1) + o가 k+2개인 수열 "m o . . . o" + S(k - 1)
+// 일단 그냥 분할 정복이용해서 N이 주어지면 k 값을 구하도록 구성하고.
+// k값을 알면 전체 길이가 몇인지 알 수 있나?,,,
+// 무조건 마지막
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    private static Map<Long, Boolean> mMap = new LinkedHashMap<>();
+    private static long n;
+    
+    public static void main(String args[]) throws IOException {
+        initialize();
+        simulate(n, 0, 0);
+    }
+    
+    private static void initialize() throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        n = Long.parseLong(br.readLine());
+    }
+    
+    private static void simulate(long num, long k, long prevLen) {
+        if (num <= 3) {
+            System.out.println(num == 1 ? "m" : "o");
+            return;
+        }
+        
+        long nowLen = prevLen * 2 + (k + 3);
+        if (nowLen < num) {
+            simulate(num, k + 1, nowLen);
+        } else { // 여기서 prevLen보다 짧은 경우는 없음 -> 즉, 2번째 공간 혹은 3번째 공간에 속함
+            if (num <= prevLen + (k + 3)) {// 2번째 영역
+                System.out.println(num - prevLen == 1 ? "m" : "o");
+                return;
+            } else { //마지막 영역
+                simulate(num - (prevLen + k + 3), 0, 0);
+            }
+        }
+    }
+}
