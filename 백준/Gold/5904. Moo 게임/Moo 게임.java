77f1eefbf2985224a3831d6fd1c9c8fa60e354e7
Lee Suyoung <78334910+twotwobread@users.noreply.github.com>
2023-06-05 16:22:51 +0900

    [Gold V] Title: Moo 게임, Time: 120 ms, Memory: 14056 KB -BaekjoonHub

--- 백준/Gold/5904. Moo 게임/Moo 게임.java
+++ 백준/Gold/5904. Moo 게임/Moo 게임.java
@@ -20,7 +20,7 @@ public class Main {
     }
     
     private static void simulate(long num, long k, long prevLen) {
-        if (num <= 3) {
+        if (k == 0 && num <= 3) {
             System.out.println(num == 1 ? "m" : "o");
             return;
         }
@@ -29,11 +29,13 @@ public class Main {
         if (nowLen < num) {
             simulate(num, k + 1, nowLen);
         } else { // 여기서 prevLen보다 짧은 경우는 없음 -> 즉, 2번째 공간 혹은 3번째 공간에 속함
-            if (num <= prevLen + (k + 3)) {// 2번째 영역
+            if (num <= prevLen) {
+                simulate(num, k - 1, (prevLen - (k + 2)) / 2);
+            } else if (prevLen < num  && num <= prevLen + (k + 3)) {// 2번째 영역
                 System.out.println(num - prevLen == 1 ? "m" : "o");
                 return;
             } else { //마지막 영역
-                simulate(num - (prevLen + k + 3), 0, 0);
+                simulate(num - (prevLen + k + 3), k - 1, (prevLen - (k + 2)) / 2);
             }
         }
     }
