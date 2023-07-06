65cd734ea5c07b0febe4b0bf71292d2474fd23b1
Lee Suyoung <78334910+twotwobread@users.noreply.github.com>
2023-07-06 18:29:08 +0900

    Update 카펫.py

--- 프로그래머스/lv2/42842. 카펫/카펫.py
+++ 프로그래머스/lv2/42842. 카펫/카펫.py
@@ -1,3 +1,5 @@
+import math
+
 def solution(brown, yellow):
     area = brown + yellow
     candidate_pairs = get_candidate_pair(area)
@@ -5,8 +7,8 @@ def solution(brown, yellow):
 
 def get_candidate_pair(area):
     result = []
-    for l in range(1, area+1):
-        for w in range(l, area+1):
+    for l in range(1, math.ceil(area**(1/2)) + 1):
+        for w in range(math.trunc(area/l), area+1):
             if area < w * l: break
             if area == w * l:
                result.append([w, l])
