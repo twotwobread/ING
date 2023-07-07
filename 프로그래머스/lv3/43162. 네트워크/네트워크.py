abedee16f188897d90dbba3f810b07b0393590f1
Lee Suyoung <78334910+twotwobread@users.noreply.github.com>
2023-07-07 16:39:04 +0900

    [level 3] Title: 네트워크, Time: 0.43 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv3/43162. 네트워크/네트워크.py
+++ 프로그래머스/lv3/43162. 네트워크/네트워크.py
@@ -1,28 +1,19 @@
 def solution(n, computers):
     result = 0
     visited = [ False for _ in range(n) ]
-    
     for i in range(n):
         if not visited[i]:
             dfs(computers, visited, i)
-            #print(visited)
             result += 1
-    
     return result
 
 def dfs(computers, visited, now):
-    #print(f"위치: {now}")
     visited[now] = True
-    
     for adj in range(len(computers[now])):
         if is_possible_go(computers, visited, now, adj): 
             dfs(computers, visited, adj)
 
 def is_possible_go(computers, visited, now, adj):
-    if now == adj or visited[adj]: 
-        #print(f"여기? now: {now}, adj: {adj}")
-        return False
-    if not computers[now][adj]: 
-        #print(f"아님 여기? : {computers[now][adj]}")
-        return False
+    if now == adj or visited[adj]: return False
+    if not computers[now][adj]: return False
     return True
